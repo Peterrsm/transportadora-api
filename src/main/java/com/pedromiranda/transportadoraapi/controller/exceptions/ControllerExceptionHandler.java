5b1bd62708b3dcc4f106048e6784f1e3d2410886
peterrsm <pedrorsmiranda@gmail.com>
2023-11-07 19:52:57 -0300

    ++ ControllerExceptionHandler

---
+++ src/main/java/com/pedromiranda/transportadoraapi/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.pedromiranda.transportadoraapi.controller.exceptions;
+
+import com.pedromiranda.transportadoraapi.service.exceptions.CaminhaoNotFoundException;
+import com.pedromiranda.transportadoraapi.service.exceptions.MotoristaNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(MotoristaNotFoundException.class)
+    public ResponseEntity<StandardError> motoristaNotFoundException(MotoristaNotFoundException ex, HttpServletRequest request) {
+        String err = "Record do not exist";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(LocalDateTime.now(), status.value(), ex.getMessage(), err, request.getRequestURI());
+
+        return ResponseEntity
+                .status(status)
+                .body(error);
+    }
+
+    @ExceptionHandler(CaminhaoNotFoundException.class)
+    public ResponseEntity<StandardError> caminhaoNotFoundException(CaminhaoNotFoundException ex, HttpServletRequest request) {
+        String err = "Record do not exist";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(LocalDateTime.now(), status.value(), ex.getMessage(), err, request.getRequestURI());
+
+        return ResponseEntity
+                .status(status)
+                .body(error);
+    }
+}
