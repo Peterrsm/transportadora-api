245f81dcc7fe6cec53cc8ed5eaeafd581c184af1
peterrsm <pedrorsmiranda@gmail.com>
2023-11-08 12:53:17 -0300

    ++ HttpMessageNotReadableException handler

--- src/main/java/com/pedromiranda/transportadoraapi/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/pedromiranda/transportadoraapi/controller/exceptions/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import com.pedromiranda.transportadoraapi.service.exceptions.CaminhaoNotFoundExc
 import com.pedromiranda.transportadoraapi.service.exceptions.MotoristaNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,6 +29,17 @@ public class ControllerExceptionHandler {
     public ResponseEntity<StandardError> caminhaoNotFoundException(CaminhaoNotFoundException ex, HttpServletRequest request) {
         String err = "Record do not exist";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(LocalDateTime.now(), status.value(), ex.getMessage(), err, request.getRequestURI());
+
+        return ResponseEntity
+                .status(status)
+                .body(error);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandardError> httpMessageNotReadableException(HttpMessageNotReadableException ex, HttpServletRequest request) {
+        String err = "Http message not readable exception";
+        HttpStatus status = HttpStatus.NOT_ACCEPTABLE;
         StandardError error = new StandardError(LocalDateTime.now(), status.value(), ex.getMessage(), err, request.getRequestURI());
 
         return ResponseEntity
